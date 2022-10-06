88745b775db9c226824338c0a33b7fc07f60bcf1
Vishnu B.S <36187277+vishnu-bs@users.noreply.github.com>
2022-10-06 15:42:45 +0200

    Update AutoConstants.java

--- src/main/java/com/fork/util/AutoConstants.java
+++ src/main/java/com/fork/util/AutoConstants.java
@@ -1,2 +1,8 @@
-package com.fork.util;public interface autoConstants {
+package com.fork.util;
+
+public interface AutoConstants {
+
+    int TIMEOUT=10;
+    String URL="https://www.thefork.fr/";
+    String FILEPATH="src/main/java/com/fork/util/user.properties";
 }
